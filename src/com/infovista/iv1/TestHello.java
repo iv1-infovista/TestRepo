a5c2613627135ecfaca49e2768476cadc8d883ec
erich <erich@hp14>
2021-05-06 10:12:13 +0200

    first commit local

---
+++ src/com/infovista/iv1/TestHello.java
@@ -0,0 +1,10 @@
+package com.infovista.iv1;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author erich
+ *
+ */
+module TestHello {
+}
