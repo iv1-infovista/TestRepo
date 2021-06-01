c0e6b6cc1b573c95f6e7bcd4b32017881c5d5669
eto <eto@mac-mini-win10>
2021-06-01 09:23:12 +0200

    little test

--- src/com/infovista/iv1/TestHello.java
+++ src/com/infovista/iv1/TestHello.java
@@ -5,6 +5,7 @@ public class TestHello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.err.println("Hello Sky");
+		System.err.println("Hello Universe");
 	}
 
 }
